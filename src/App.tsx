cc1ae1f2623d52e67d2d796414bbe0384a629a23
waszuch <140809966+waszuch@users.noreply.github.com>
2024-10-09 18:41:19 +0200

    building  layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+
 import './style.css'
+import kotekDeszcz from './assets/kotek-deszcz.png'
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app-wrapper min-h-screen flex items-center justify-center">
+      <div className="app-container w-[90vw] h-[80vh] border border-black rounded-lg flex">
+        <div className="kitty-image w-[40%] h-full flex items-center justify-center">
+          <img src={kotekDeszcz} className="w-[90%] h-[90%] object-cover rounded-lg" />
+        </div>
+        <div className="weather-content w-[60%] h-full">
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  )}
 
 export default App
