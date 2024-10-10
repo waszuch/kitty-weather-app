af3f80bcb9321b1d8e357e2e21f90d1d2881e4ee
waszuch <140809966+waszuch@users.noreply.github.com>
2024-10-10 07:59:47 +0200

    mobile responisve change

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,19 @@ import CurrentWeather from './components/CurrentWeather'
 function App() {
   return (
     <div className="app-wrapper min-h-screen flex items-center justify-center">
-      <div className="app-container w-[90vw] h-[90vh] border border-black rounded-lg flex">
-        <div className="kitty-image w-[40%] h-full flex items-center justify-center">
+      <div className="app-container w-[90vw] h-[90vh] border border-black rounded-lg flex flex-col sm:flex-row">
+        <div className="kitty-image w-full h-[40%] sm:w-[40%] sm:h-full flex items-center justify-center">
           <img src={kotekDeszcz} className="w-[90%] h-[90%] object-cover rounded-lg" />
         </div>
-        <div className="weather-content w-[60%] h-full flex flex-col items-start justify-start">
-          <div className="top w-full flex flex-col items-center mt-4"> 
-            <SearchBar />
-            <div className="mt-4"/>
-            <CurrentWeather />
+        <div className="weather-content w-full h-[60%] sm:w-[60%] sm:h-full flex flex-col items-start justify-start">
+          <div className="top w-full flex flex-col items-center mt-4">
+            <div className="w-full h-[10%] sm:h-auto justify-center flex items-center">
+              <SearchBar />
+            </div>
+            <div className="mt-4" />
+            <div className="w-full h-[25%] sm:h-auto">
+              <CurrentWeather />
+            </div>
           </div>
         </div>
       </div>
